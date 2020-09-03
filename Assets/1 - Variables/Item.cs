e41a7d686ce0360d55c8b6d30e3fcb2512c3122f
Iseta <isabelle.brandelero@gmail.com>
2020-09-03 12:07:11 -0300

    Commit inicial

---
+++ Assets/1 - Variables/Item.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Item : MonoBehaviour
+{
+    //name
+    public string itemName;
+    //description
+    [TextArea]
+    public string about;
+    //icon
+    public Sprite image;
+    //attack
+    public float strength;
+    //price
+    public int price;
+
+    void Start()
+    {
+        Debug.Log("I am a " + itemName + " about me: " + about);
+        Debug.Log("I deal " + strength + " damage and I cost " + price +" gold");
+    }
+}
---
+++ Assets/1 - Variables/MasterVariablesP1.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MasterVariablesP1 : MonoBehaviour
+{
+    //create a variable to store your name
+    public string myName;
+
+    //create a variable to store your age
+    public int myAge;
+
+    //create a health variable
+    public int health;
+
+    //create a score variable
+    public int score;
+
+    //create a variable for ammo count
+    public int ammoCount;
+
+    //create a variable to store your speed
+    public float speed;
+
+    //create a variable to check if all keys were collected
+    public bool allKeysColected;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        Debug.Log("My name is: " + myName + ", my age is: " + myAge + " and I am: "+ speed +" units fast");
+        Debug.Log("My current health is: " + health + ", my score is: " + score +" and I have: "+ammoCount+" of ammo");
+        Debug.Log("Do I have all my keys? " + allKeysColected);
+    }
+
+}
