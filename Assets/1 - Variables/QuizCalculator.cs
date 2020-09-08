71cb5df6ddd89a5bbb4699faea5649450bcc4d64
Iseta <isabelle.brandelero@gmail.com>
2020-09-08 15:07:06 -0300

    Módulo de variáveis terminado

---
+++ Assets/1 - Variables/QuizCalculator.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class QuizCalculator : MonoBehaviour
+{
+    public int gradesLength;
+    public float[] grades;
+    private float average;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        grades = new float[gradesLength];
+        for (int i = 0; i < gradesLength; i++)
+        {
+            grades[i] = Random.Range(0.0f, 10.0f);
+            average += grades[i];
+        }
+        average = average / gradesLength;
+        Debug.Log("Your average grade in scores is: "+average);
+    }
+}
---
+++ Assets/1 - Variables/TipCalculator.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TipCalculator : MonoBehaviour
+{
+    //bill amount
+    public float billAmount;
+
+    //tip percentage
+    public float tipPercentage;
+
+    //total amount = bill+tip
+    float totalAmount;
+
+    void Start()
+    {
+        totalAmount = billAmount + ((tipPercentage / 100) * billAmount);
+        Debug.Log("Your bill is: " + billAmount + ", and the total amount, with a " + tipPercentage + "% tip is: " + totalAmount);
+    }
+
+}
