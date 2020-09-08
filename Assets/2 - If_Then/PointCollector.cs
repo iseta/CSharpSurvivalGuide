b5f3d4bbf4fcf473be14a0f07d6dbe562a528fd9
Iseta <isabelle.brandelero@gmail.com>
2020-09-08 16:18:07 -0300

    Começo do módulo de IF ELSE

---
+++ Assets/2 - If_Then/PointCollector.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PointCollector : MonoBehaviour
+{
+    public int pointsToAdd;
+    public int totalPoints;
+    private bool hasSaidMessage;
+
+    void Update()
+    {
+        //Whenever the space key is pressed, add x points
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            totalPoints += pointsToAdd;
+            if (totalPoints >= 50 && !hasSaidMessage)
+            {
+                hasSaidMessage = true;
+                Debug.Log("You're awesome!");
+            }
+        }
+    }
+}
