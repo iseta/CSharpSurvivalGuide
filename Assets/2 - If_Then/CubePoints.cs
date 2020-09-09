0228d14346208d3d214d755038e756e24f7f7db0
Iseta <isabelle.brandelero@gmail.com>
2020-09-09 14:54:23 -0300

    Modulo IF_Then finalizado

---
+++ Assets/2 - If_Then/CubePoints.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CubePoints : MonoBehaviour
+{
+    //Gameobject 
+    public GameObject cube;
+
+    //Score value
+    private int _score;
+
+    //Was color changed
+    private bool _wasColorChanged;
+
+    void Start()
+    {
+        cube.GetComponent<Renderer>().material.color = Color.red;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            _score += 10;
+            if (_score > 50 && !_wasColorChanged)
+            {
+                cube.GetComponent<Renderer>().material.color = Color.green;
+                _wasColorChanged = true;
+            }
+        }
+    }
+}
---
+++ Assets/2 - If_Then/SpeedControl.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpeedControl : MonoBehaviour
+{
+    //Speed variable
+    private int _speed = 10;
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.D))
+        {
+            _speed += 10;
+            if (_speed > 20)
+            {
+                Debug.Log("Slow down!");
+            }
+        }
+
+        if (Input.GetKeyDown(KeyCode.A))
+        {
+            if (_speed > 0)
+            {
+                _speed -= 10;
+            } else if (_speed == 0)
+            {
+                Debug.Log("Speed up!");
+            }
+        }
+    }
+}
