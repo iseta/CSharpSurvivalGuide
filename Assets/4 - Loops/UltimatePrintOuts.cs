e05c1ae3f7419adacc761dba1f1a51adcfa432b9
Iseta <isabelle.brandelero@gmail.com>
2020-09-16 15:48:03 -0300

    Começo do módulo de Loops

---
+++ Assets/4 - Loops/UltimatePrintOuts.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class UltimatePrintOuts : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        for(int i = 0; i < 30; i++)
+        {
+            if (i < 10)
+            {
+                Debug.Log(i);
+            }else if(i >= 10 && i<20 && (i%2 == 0))
+            {
+                Debug.Log(i);
+            }else if(i>=20 && (i % 2 == 1))
+            {
+                Debug.Log(i);
+            }
+        }
+    }
+}
