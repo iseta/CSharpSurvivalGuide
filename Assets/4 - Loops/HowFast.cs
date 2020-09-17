139380405b476ebf27cf5fb64503f9698666198b
Iseta <isabelle.brandelero@gmail.com>
2020-09-17 10:52:00 -0300

    Concluído o módulo de Loops

---
+++ Assets/4 - Loops/HowFast.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HowFast : MonoBehaviour
+{
+    public int maxSpeed;
+    [SerializeField]
+    private int _currentSpeed;
+
+    void Start()
+    {
+        StartCoroutine(incrementSpeed());
+    }
+
+    IEnumerator incrementSpeed()
+    {
+        while(_currentSpeed < maxSpeed)
+        {
+            _currentSpeed += 5;
+            yield return new WaitForSeconds(5);
+        }
+    }
+
+}
