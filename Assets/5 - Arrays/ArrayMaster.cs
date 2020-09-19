bd26eacdb6f0f276d575d2b577f010e820fcac61
Iseta <isabelle.brandelero@gmail.com>
2020-09-19 14:13:18 -0300

    Iniciado o módulo de arrays

---
+++ Assets/5 - Arrays/ArrayMaster.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ArrayMaster : MonoBehaviour
+{
+    //Array with kids names
+    public string[] names;
+
+    //Array with their ages
+    public int[] ages;
+
+    //Array with their favorite cars
+    public string[] favoriteCars;
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Space)){
+            if(names!=null && favoriteCars != null)
+            {
+                Debug.Log(names[names.Length -1] + " is "+ages[ages.Length -1]+" year(s) old and their favorite car is: " + favoriteCars[favoriteCars.Length -1]);
+            }
+        }
+    }
+}
---
+++ Assets/5 - Arrays/ArrayRandomlySelect.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ArrayRandomlySelect : MonoBehaviour
+{
+    //Array with kids names
+    public string[] names;
+
+    //Array with their ages
+    public int[] ages;
+
+    //Array with their favorite cars
+    public string[] favoriteCars;
+
+    private int _randomIndex;
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            if (names != null && favoriteCars != null)
+            {
+                _randomIndex = Random.Range(0, names.Length - 1);
+                Debug.Log(names[_randomIndex] + " is " + ages[_randomIndex] + " year(s) old and their favorite car is: " + favoriteCars[_randomIndex]);
+            }
+        }
+    }
+}
