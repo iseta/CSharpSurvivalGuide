ee3eb8a4afb0238d015e9a45f6a4102066e92663
Iseta <isabelle.brandelero@gmail.com>
2020-09-21 10:11:37 -0300

    Concluído o módulo de arrays

---
+++ Assets/5 - Arrays/ConvertCubes.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ConvertCubes : MonoBehaviour
+{
+    public GameObject[] cubes;
+
+    // The challenge was to turn cubes red using for loops, and not foreach loops, which would've been more efficient
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            for(int i = 0; i<cubes.Length; i++)
+            {
+                cubes[i].GetComponent<Renderer>().material.color = Color.red;
+            }
+        }
+
+        if (Input.GetKeyUp(KeyCode.Space))
+        {
+            for (int i = 0; i < cubes.Length; i++)
+            {
+                cubes[i].GetComponent<Renderer>().material.color = Color.white;
+            }
+        }
+    }
+}
