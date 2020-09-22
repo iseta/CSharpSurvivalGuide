360938101315789a58dc8ec524618e909e5c5abb
Iseta <isabelle.brandelero@gmail.com>
2020-09-22 10:33:01 -0300

    Iniciado o módulo de classes

---
+++ Assets/7 - Classes/Customer.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[System.Serializable]
+public class Customer
+{
+    public string firstName;
+    public string lastName;
+    public int age;
+    public char gender;
+    public string occupation;
+
+    public Customer(string firstName, string lastName, int age, char gender, string occupation)
+    {
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.age = age;
+        this.gender = gender;
+        this.occupation = occupation;
+    }
+}
---
+++ Assets/7 - Classes/CustomerDatabase.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CustomerDatabase : MonoBehaviour
+{
+    public Customer[] customers;
+
+    void Start()
+    {
+        customers[0] = new Customer("Brad", "Shaw", 34, 'M', "Programmer");
+        customers[1] = new Customer("Molly", "Evans", 14, 'F', "Student");
+        customers[2] = new Customer("Agnes", "DiPiero", 54, 'F', "Retired");
+    }
+}
