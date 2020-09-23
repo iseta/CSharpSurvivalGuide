bec0a020bc929763a65e76288c226a5ec0562942
Iseta <isabelle.brandelero@gmail.com>
2020-09-23 16:29:14 -0300

    Iniciado e concluído os módulos de Static Types

---
+++ Assets/8 - Static Types/ColorHelper.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public static class ColorHelper
+{
+    public static void ChangeColor(GameObject g, Color color, bool isRandomized){
+        if (isRandomized)
+        {
+            color = new Color(Random.Range(0f, 1f), Random.Range(0f, 1f), Random.Range(0f, 1f));
+        }
+        g.GetComponent<Renderer>().material.color = color;
+    }
+}
---
+++ Assets/ColorManager.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ColorManager : MonoBehaviour
+{
+    public GameObject sphere;
+    public GameObject cube;
+    public GameObject cylinder;
+    public GameObject capsule;
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.W))
+        {
+            ColorHelper.ChangeColor(sphere, Color.white, true);
+        }
+        if (Input.GetKeyDown(KeyCode.A))
+        {
+            ColorHelper.ChangeColor(cube, Color.white, true);
+        }
+        if (Input.GetKeyDown(KeyCode.S))
+        {
+            ColorHelper.ChangeColor(cylinder, Color.white, true);
+        }
+        if (Input.GetKeyDown(KeyCode.D))
+        {
+            ColorHelper.ChangeColor(capsule, Color.white, true);
+        }
+    }
+}
