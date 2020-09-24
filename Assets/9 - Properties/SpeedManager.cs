e3cea3b2a5676cce45d32140eb2d79e1862fdcea
Iseta <isabelle.brandelero@gmail.com>
2020-09-24 10:52:05 -0300

    Iniciado e finalizado o módulo de Static Types

---
+++ Assets/9 - Properties/SpeedManager.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpeedManager : MonoBehaviour
+{
+    private int _speed;
+
+    public int speed
+    {
+        get
+        {
+            Debug.Log("My speed is: ");
+            return _speed;
+        }
+    }
+
+    private string objectName { get; set; }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
