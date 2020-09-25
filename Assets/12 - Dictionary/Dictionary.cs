e7e69c29f31110ef288c6608fc65c9af4f150189
Iseta <isabelle.brandelero@gmail.com>
2020-09-25 14:51:32 -0300

    Iniciado e finalizado o módulo de dicionários

---
+++ Assets/12 - Dictionary/Dictionary.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Dictionary : MonoBehaviour
+{
+    public List<ItemD> itemList = new List<ItemD>();
+    public Dictionary<int, ItemD> itemDictionary = new Dictionary<int, ItemD>();
+
+    private void Start()
+    {
+        ItemD sword = new ItemD();
+        sword.name = "Sword";
+        sword.id = 0;
+
+        itemList.Add(sword);
+        itemDictionary.Add(0, sword);
+
+        ItemD newItem = itemDictionary[0];
+        Debug.Log(newItem.name);
+    }
+}
---
+++ Assets/12 - Dictionary/ItemD.cs
@@ -0,0 +1,10 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ItemD : MonoBehaviour
+{
+    public string itemName;
+    public int id;
+    public Sprite icon;
+}
