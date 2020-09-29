0d06a193835b72e7beb124f38c54d07fb1fcde29
Iseta <isabelle.brandelero@gmail.com>
2020-09-29 10:57:49 -0300

    Início do módulo Delegates and Events

---
+++ Assets/14 - Delegates and Events/Cube.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Cube : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        MainDelegate.onTeleport += TeleportCube;
+    }
+
+    public void TeleportCube(Vector3 pos)
+    {
+        gameObject.transform.position = pos;
+    }
+
+    private void OnDisable()
+    {
+        MainDelegate.onTeleport -= TeleportCube;
+    }
+}
---
+++ Assets/14 - Delegates and Events/MainDelegate.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MainDelegate : MonoBehaviour
+{
+    public delegate void Teleport(Vector3 pos);
+    public static event Teleport onTeleport;
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            onTeleport?.Invoke(new Vector3(5, 2, 0));
+        }
+    }
+}
